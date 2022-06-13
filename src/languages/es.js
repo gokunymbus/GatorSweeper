9929b0c51224ac76594849faa1a929dcb60e987c
John Miller <622498+gokunymbus@users.noreply.github.com>
2022-06-12 22:48:03 -0400

    Adding missing keys to other langauges, still need them translated.

--- src/languages/es.js
+++ src/languages/es.js
@@ -5,5 +5,21 @@ export default {
     "controlsGameStateStarted": "¡Empezada!",
     "controlsGameStateFailed": "¡Ha fallado!",
     "controlsGameStateNew": "¡Hacer clic!",
-    "controlsGameStateWinner": "¡Ganadora!"
+    "controlsGameStateWinner": "¡Ganadora!",
+    "controlsResetButton": "Reset game",
+    "controlsNumberOfFlags": "Number of flags",
+    "controlsSeconds": "Seconds have passed",
+    "controlsAEDGameStateWinner": "You have won the game, congradulations. Press 1 to play again",
+    "controlsAEDGameStateNew": "New game, press the 2 key to focus the first Tile in the grid.",
+    "controlsAEDGameStateFailed": "You have failed the game, press the 1 key to play again.",
+    "controlsAEDGameStateStarted": "The game has started.",
+    "gridAED": "Main Grid. Contains rows and columns with tiles in each cell. Once a tile is selected, use the arrow keys to navigate and Enter to uncover a tile. Press {0} to flag a tile if you think a gator is there.",
+    "tileAED": "Tile row {0}, column {1}.",
+    "tileAEDRevealedMine": "It's revealed and is a gator, game over.",
+    "tileAEDRevealed": "Revealed with {0} gators in proximity.",
+    "tileAEDHidden": "Not Revealed",
+    "tileAEDIsFlagged": "Flagged",
+    "footerAEDEasy": "Set game to Easy mode.",
+    "footerAEDHard": "Set Game to Hard mode, it's pretty hard.",
+    "footerAEDExtreme": "Set game to the most Extreme mode."
 }
