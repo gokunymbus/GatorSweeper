9b487f21640e9c71e6937d36c9143f95296062e9
John Miller <622498+gokunymbus@users.noreply.github.com>
2022-06-23 11:29:13 -0400

    Updating capitlization and some minor linting.

--- src/ui/Tile.jsx
+++ src/ui/Tile.jsx
@@ -18,7 +18,7 @@ import Language from "../languages/Language";
 import {setFlagKey, selectTileKey} from "../library/Constants";
 
 // Utilities
-import replaceStringTokens from "../utilities/replaceStringTokens";
+import replaceStringTokens from "../utilities/replaceStringToken";
 import timer from "../utilities/Timer";
 
 export const testID = "tile";
